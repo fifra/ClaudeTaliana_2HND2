b2862bdb673224f9dae2813145fba9e4064ef90d
fifra <fifra0@gmail.com>
2014-06-10 19:59:56 +0200

    Update Quit
    Changed button to Esc key

--- Assets/Scripts/quit.js
+++ Assets/Scripts/quit.js
@@ -7,7 +7,7 @@ function Start () {
 
 // quit game if Q button is pressed
 function Update () {
-	if(Input.GetKey(KeyCode.Q)){
+	if(Input.GetKey(KeyCode.Escape)){
 		Application.LoadLevel("Main"); //quit the game
 	}
 }
